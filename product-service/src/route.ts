fb328e65aee777e0133d47df711d16554e0a6fba
Joe Kim <joesm.genie@gmail.com>
2023-12-21 16:08:25 +0700

    Implement try catch

--- product-service/src/route.ts
+++ product-service/src/route.ts
@@ -5,37 +5,62 @@ import { updateProduct, createProduct, getAllProducts, getProduct, deleteProduct
 const router: Router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-  const products = await getAllProducts();
-  res.status(200).json(products).send();
+  try {
+    const products = await getAllProducts();
+    res.status(200).json(products).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
 });
 
 router.get('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id as string;
-  const product = await getProduct(parseInt(id));
-  if (!product) {
-    res.status(400).json({ msg: 'Product not found' }).send();
-    return;
+  try {
+    const id = req.params.id as string;
+    const product = await getProduct(parseInt(id));
+    if (!product) {
+      res.status(400).json({ msg: 'Product not found' }).send();
+      return;
+    }
+    res.status(200).json(product).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
   }
-  res.status(200).json(product).send();
 });
 
 router.post('/', async (req: Request, res: Response) => {
-  const input: Prisma.ProductCreateInput = req.body;
-  const product = await createProduct(input);
-  res.status(201).json(product).send();
+  try {
+    const input: Prisma.ProductCreateInput = req.body;
+    const product = await createProduct(input);
+    res.status(201).json(product).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
 });
 
 router.patch('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  const input: Prisma.ProductUpdateInput = req.body;
-  const product = await updateProduct(parseInt(id), input);
-  res.status(200).json(product).send();
+  try {
+    const id = req.params.id;
+    const input: Prisma.ProductUpdateInput = req.body;
+    const product = await updateProduct(parseInt(id), input);
+    res.status(200).json(product).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
 });
 
 router.delete('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  await deleteProduct(parseInt(id));
-  res.status(204).send();
+  try {
+    const id = req.params.id;
+    await deleteProduct(parseInt(id));
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
 });
 
 export default router;
