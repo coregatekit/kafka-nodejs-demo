e853e25bf460a48be46dd769aafba0f23a1a99f1
Joe Kim <joesm.genie@gmail.com>
2023-12-21 17:29:17 +0700

    Implement loggin for product service

--- product-service/src/server.ts
+++ product-service/src/server.ts
@@ -1,10 +1,12 @@
 import express, { json, Request, Response, Application } from 'express';
+import morgan from 'morgan';
 import router from './route';
 import { CURRENT_HOST } from './config';
 
 const app: Application = express();
 app.listen(9001, () => console.log(`${CURRENT_HOST} Product Server is running on port 9001`));
 app.use(json());
+app.use(morgan('combined'));
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Hello Product Service');
