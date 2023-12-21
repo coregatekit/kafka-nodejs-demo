032d047a1bf3ed3b6f4951bbec8ba996a1b9734f
Joe Kim <joesm.genie@gmail.com>
2023-12-21 15:37:48 +0700

    Implement find a product

--- product-service/src/route.ts
+++ product-service/src/route.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from 'express';
-import { getAllProducts } from './service';
+import { getAllProducts, getProduct } from './service';
 
 const router: Router = Router();
 
@@ -8,9 +8,13 @@ router.get('/', async (req: Request, res: Response) => {
   res.status(200).json(products).send();
 });
 
-router.get('/:id', (req: Request, res: Response) => {
-  const id = req.params.id;
-  res.status(200).send(`Get product id = ${id}`);
+router.get('/:id', async (req: Request, res: Response) => {
+  const id = req.params.id as string;
+  const product = await getProduct(parseInt(id));
+  if (!product) {
+    res.status(400).json({ msg: 'Product not found' }).send();
+  }
+  res.status(200).json(product).send();
 });
 
 router.post('/', (req: Request, res: Response) => {
--- product-service/src/service.ts
+++ product-service/src/service.ts
@@ -4,6 +4,11 @@ async function getAllProducts() {
   return await prisma.product.findMany();
 }
 
+async function getProduct(id: number) {
+  return await prisma.product.findFirst({ where: { id } });
+}
+
 export {
   getAllProducts,
+  getProduct,
 }
